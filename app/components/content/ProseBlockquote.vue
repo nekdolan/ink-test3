41294c9ecc5d08d507b3b4ffcabbc338cddb0152
Daniel <boros.daniel@protonmail.com>
2025-09-24 20:32:41 +0200

    added video files

---
+++ app/components/content/ProseBlockquote.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+  <blockquote class="blockquote -left -bordered _text:italic">
+    <slot></slot>
+  </blockquote>
+</template>
---
+++ app/components/content/ProseImg.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps(['src', 'alt']);
+</script>
+<template>
+  <nuxt-img
+      :src="props.src"
+      :alt="props.alt || ''"
+      format="webp"
+      densities="1x"
+      class="_image:responsive"
+  />
+</template>
